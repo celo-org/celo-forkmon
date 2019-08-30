87a058ff3dabcd0f2a017be32613fb0619579308
soc1c <soc1c@users.noreply.github.com>
2019-08-30 10:38:45 +0200

    add public endpoint

--- app/chainstate/config.py
+++ app/chainstate/config.py
@@ -15,15 +15,15 @@ nodes_debug = {
 
 nodes_prod = {
     'Geth Classic (Atlantis)': {
-    	'url': 'http://127.0.0.1:8545',
+    	'url': 'http://51.15.97.240:8545',
     	'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
     },
     'Multi Geth (Atlantis)': {
-        'url': 'http://127.0.0.1:9545',
+        'url': 'http://51.15.97.240:9545',
         'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
     },
     'Parity Ethereum (Atlantis)': {
-        'url': 'http://127.0.0.1:10545',
+        'url': 'http://51.15.97.240:10545',
         'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
     },
 }
