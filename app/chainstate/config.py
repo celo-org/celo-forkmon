1d2bdb00df2b6c4419c11744c7a767c414c0d821
/raw PONG _GHMoaCXLT <58883403+q9f@users.noreply.github.com>
2020-04-11 17:20:13 +0200

    provide phoenix nodes for classic

--- app/chainstate/config.py
+++ app/chainstate/config.py
@@ -1,37 +1,80 @@
 nodes_debug = {
-    'Geth Classic (Atlantis)': {
-    	'url': 'http://localhost:8545',
-    	'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Multi Geth (Atlantis)': {
-        'url': 'http://localhost:9545',
+    'Hyperledger Besu,Phoenix': {
+        'url': 'http://localhost:8555',
         'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
     },
-    'Parity Ethereum (Atlantis)': {
-        'url': 'http://localhost:10545',
+    'Core-Geth,Phoenix': {
+        'url': 'http://localhost:8565',
         'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
     },
-    'Parity Ethereum (Legacy)': {
-        'url': 'http://localhost:18545',
+    'Open Ethereum,Aztlan Fix': {
+        'url': 'http://localhost:8545',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+    'Multi-Geth,Phoenix': {
+        'url': 'http://localhost:8575',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+    'Geth Classic,Agharta': {
+        'url': 'http://localhost:8585',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+    'IOHK Mantis,Legacy': {
+        'url': 'http://localhost:8595',
         'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
     },
 }
 
-nodes_prod = {
-    'Geth Classic (Atlantis)': {
-    	'url': 'http://163.172.154.197:8545',
-    	'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Multi Geth (Atlantis)': {
-        'url': 'http://163.172.154.197:9545',
+nodes_production = {
+    'Hyperledger Besu,Phoenix': {
+        'url': 'http://merga.sclwy.fault.dev:8555',
         'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
     },
-    'Parity Ethereum (Atlantis)': {
-        'url': 'http://163.172.154.197:10545',
+    'Core-Geth,Phoenix': {
+        'url': 'http://merga.sclwy.fault.dev:8565',
         'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
     },
-    'Parity Ethereum (Legacy)': {
-        'url': 'http://163.172.154.197:18545',
+    'Open Ethereum,Aztlan Fix': {
+        'url': 'http://merga.sclwy.fault.dev:8545',
         'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
     },
+    'Multi-Geth,Phoenix': {
+        'url': 'http://merga.sclwy.fault.dev:8575',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+    'Geth Classic,Agharta': {
+        'url': 'http://merga.sclwy.fault.dev:8585',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+#   'IOHK Mantis,Legacy': {
+#       'url': 'http://merga.sclwy.fault.dev:8595',
+#       'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+#   },
+}
+
+nodes_fallback = {
+    'Hyperledger Besu,Phoenix': {
+        'url': 'http://ceibo.htznr.fault.dev:8555',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+    'Core-Geth,Phoenix': {
+        'url': 'http://ceibo.htznr.fault.dev:8565',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+    'Open Ethereum,Aztlan Fix': {
+        'url': 'http://ceibo.htznr.fault.dev:8545',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+    'Multi-Geth,Phoenix': {
+        'url': 'http://ceibo.htznr.fault.dev:8575',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+    'Geth Classic,Agharta': {
+        'url': 'http://ceibo.htznr.fault.dev:8585',
+        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    },
+#   'IOHK Mantis,Legacy': {
+#       'url': 'http://ceibo.htznr.fault.dev:8595',
+#       'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+#   },
 }
