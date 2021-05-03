a6a2156d4ac2164d79438f8fb05a5db76ae919e0
YazzyYaz <yaz.khoury@gmail.com>
2021-05-03 16:51:08 -0400

    feat: Initial modifications for Celo Forkmon

--- app/chainstate/config.py
+++ app/chainstate/config.py
@@ -1,68 +1,13 @@
-nodes_debug = {
-    'Hyperledger Besu,Phoenix,v1.4.3-stable-3f4f14c1': {
-        'url': 'http://localhost:8555',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Core-Geth,Phoenix,v1.11.2-stable-cbe4a649': {
-        'url': 'http://localhost:8565',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Open Ethereum,Phoenix,v3.0.0-nightly-56885fe4': {
-        'url': 'http://localhost:8545',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Multi-Geth,Phoenix,v1.9.14-stable-3f141bb4': {
-        'url': 'http://localhost:8575',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Geth Classic,Agharta,v6.1.2-stable-71d8b42b': {
-        'url': 'http://localhost:8585',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-}
-
 nodes_production = {
-    'Hyperledger Besu,Phoenix,v1.4.3-stable-3f4f14c1': {
-        'url': 'http://merga.sclwy.fault.dev:8555',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Core-Geth,Phoenix,v1.11.2-stable-cbe4a649': {
-        'url': 'http://merga.sclwy.fault.dev:8565',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Open Ethereum,Phoenix,v3.0.0-nightly-56885fe4': {
-        'url': 'http://merga.sclwy.fault.dev:8545',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Multi-Geth,Phoenix,v1.9.14-stable-3f141bb4': {
-        'url': 'http://merga.sclwy.fault.dev:8575',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Geth Classic,Agharta,v6.1.2-stable-71d8b42b': {
-        'url': 'http://merga.sclwy.fault.dev:8585',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+    'Celo Blockchain,(Non-Donut Activated),v1.2.5': {
+        'url': 'https://forno.celo.org',
+        'explorer': "https://explorer.celo.org/blocks/%s",
     },
 }
 
-nodes_fallback = {
-    'Hyperledger Besu,Phoenix,v1.4.3-stable-3f4f14c1': {
-        'url': 'http://ceibo.htznr.fault.dev:8555',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Core-Geth,Phoenix,v1.11.2-stable-cbe4a649': {
-        'url': 'http://ceibo.htznr.fault.dev:8565',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Open Ethereum,Phoenix,v3.0.0-nightly-56885fe4': {
-        'url': 'http://ceibo.htznr.fault.dev:8545',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Multi-Geth,Phoenix,v1.9.14-stable-3f141bb4': {
-        'url': 'http://ceibo.htznr.fault.dev:8575',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
-    },
-    'Geth Classic,Agharta,v6.1.2-stable-71d8b42b': {
-        'url': 'http://ceibo.htznr.fault.dev:8585',
-        'explorer': "https://blockscout.com/etc/mainnet/blocks/%s",
+nodes_debug = {
+    'Celo Blockchain,(Non-Donut Activated),v1.2.5': {
+        'url': 'https://forno.celo.org',
+        'explorer': "https://explorer.celo.org/blocks/%s",
     },
 }
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import datetime
 import web3
 
 from web3 import Web3, HTTPProvider
+from web3.middleware import geth_poa_middleware
 from flask import Flask, request, make_response, send_file
 import heapq
 import json
@@ -13,11 +14,6 @@ graph_length = 16
 block_interval_average_len = 500
 cache_duration = 3600
 cache_blocks = 1000
-fork_total_difficulty = 934691916802069961084.0
-max_hash_rate = 1
-max_difficulty = 1
-max_total_difficulty = 1
-fork_hash_rate = 9046117559975.0
 app = Flask(__name__)
 app.config['PROPAGATE_EXCEPTIONS'] = True
 
@@ -25,7 +21,6 @@ def get_nodes():
     if app.debug:
         return config.nodes_debug
     else:
-#       return config.nodes_fallback
         return config.nodes_production
 
 def hash_of(blockOrHash):
@@ -45,20 +40,15 @@ def to_dict(block):
     retain the bare essentials in memory, also converting hashes (ByteArray) into
     regular hex-strings
     """
-    uncles = [hash_of(u) for u in block['uncles']]
     parentHash = hash_of(block['parentHash'])
     return {
         'number': block['number'],
         'timestamp': block['timestamp'],
         'hash': hash_of(block),
-        'difficulty': block['difficulty'],
-        'totalDifficulty': block['totalDifficulty'],
         'size': block['size'],
         'gasUsed': block['gasUsed'],
         'parentHash' : parentHash,
-        'gasLimit': block['gasLimit'],
-        'uncles': uncles,
-        'parents': [parentHash] + uncles,
+        'parents': [parentHash],
     }
 
 clients = {}
@@ -72,6 +62,7 @@ class BlockFetcher(object):
         self.cache_duration = cache_duration
         self.cache_blocks = cache_blocks
         self.client = client
+        self.client.middleware_onion.inject(geth_poa_middleware, layer=0)
         self.block_hash_heap = []
         self.block_hash_cache = {}
         self.block_number_cache = {}
@@ -145,9 +136,6 @@ def find_ancestors(roots, earliest):
             if ts >= earliest:
                 if hash_of(block['parentHash']) not in blocks:
                     frontier.add(block['parentHash'])
-                for uncle in block['uncles']:
-                    if uncle not in blocks:
-                        frontier.add(uncle)
     return blocks
 
 def build_block_graph(roots, earliest):
@@ -167,14 +155,10 @@ def build_block_info(clientname):
     latest = get_latest_block(clientname)
     latestNumber = int(latest['number'])
     latestTimestamp = latest['timestamp']
-    difficulty = latest['difficulty']
-    totalDiff = latest['totalDifficulty'] - fork_total_difficulty
     blockInterval = 0
-    hashRate = 0
     diffStyle = "text-muted"
     tdStyle = "text-muted"
     intervalStyle = "text-muted"
-    hashRateStyle = "text-muted"
     earlierNumber = latestNumber - block_interval_average_len
     if earlierNumber > 0:
         try:
@@ -183,36 +167,13 @@ def build_block_info(clientname):
             return None
         earlierTimestamp = earlier['timestamp']
 
-        difficulty = latest['difficulty']
         blockInterval = (latestTimestamp - earlierTimestamp) / float(block_interval_average_len)
-        hashRate = difficulty / blockInterval
-    relDiff = 100 * difficulty / float(max_difficulty)
-    if relDiff >= 100:
-        diffStyle = "text-success"
-    elif relDiff >= 80:
-        diffStyle = "text-warning"
-    else:
-        diffStyle = "text-danger"
-    relTD = 100 * totalDiff / float(max_total_difficulty)
-    if relTD >= 90:
-        tdStyle = "text-success"
-    elif relTD >= 50:
-        tdStyle = "text-warning"
-    else:
-        tdStyle = "text-danger"
     if blockInterval <= 15 and blockInterval > 0:
         intervalStyle = "text-success"
     elif blockInterval <= 30 and blockInterval > 0:
         intervalStyle = "text-warning"
     else:
         intervalStyle = "text-danger"
-    relHashRate = hashRate / float(max_hash_rate) * 100
-    if relHashRate >= 80:
-        hashRateStyle = "text-success"
-    elif relHashRate >= 50:
-        hashRateStyle = "text-warning"
-    else:
-        hashRateStyle = "text-danger"
     clientversion = clientname.split(',')[2]
     clientfork = clientname.split(',')[1]
     forkStyle = "text-muted"
@@ -234,10 +195,7 @@ def build_block_info(clientname):
         'timestamp': latestTimestamp,
         'hash': latest['hash'],
         'shortHash': latest['hash'][:10],
-        'difficulty': difficulty,
-        'totalDifficulty': totalDiff,
         'blockInterval': "%.1f" % (blockInterval,),
-        'hashRate': "%.1f" % (hashRate / 1000000000),
         'name': clientname,
         'clean': cleanname,
         'fork': clientfork,
@@ -247,21 +205,11 @@ def build_block_info(clientname):
         'diffStyle': diffStyle,
         'tdStyle': tdStyle,
         'intervalStyle': intervalStyle,
-        'hashRateStyle': hashRateStyle,
     }
 
 def build_block_infos():
-    global max_hash_rate
-    global max_difficulty
-    global max_total_difficulty
     infos = [build_block_info(name) for name in get_nodes()]
     infos = [x for x in infos if x != None]
-    max_hash_rate = float(max(info['hashRate'] for info in infos))
-    max_difficulty = float(max(info['difficulty'] for info in infos))
-    max_total_difficulty = max(info['totalDifficulty'] for info in infos)
-    for info in infos:
-        info['difficulty'] = "%.2f" % (100 * info['difficulty'] / float(max_difficulty),)
-        info['totalDifficulty'] = "%.2f" % (100 * info['totalDifficulty'] / float(max_total_difficulty),)
     return infos
 
 @app.route('/')
